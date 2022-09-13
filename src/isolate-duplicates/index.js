a4a9395b73161fe23e4d6bb0272957e329391ed5
Mercyogbenjuwa <99675525+Mercyogbenjuwa@users.noreply.github.com>
2022-09-13 15:00:43 +0100

    Update index.js

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,3 +1,24 @@
-function isolateDuplicates(text) {}
+function isolateDuplicates(text) {
+	
+		let s = new Set();
+		
+		
+		for (let i = 0;i<n;i++)
+			s.add(str[i]);
+
+		
+		for (const v of s) {
+
+			document.write(v);
+	}
+	}
+
+	
+		let text = "aisha";
+		text = str.length;
+
+		isolateDuplicates(str, n);
+
+}
 
 module.exports = isolateDuplicates;
